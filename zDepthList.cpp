58c4bf660d29e028c109ed537c55099abf815edd
keithdawson <blakemizzell@gmail.com>
2017-03-22 20:07:22 -0500

    Update.

--- zDepthList.cpp
+++ zDepthList.cpp
@@ -8,14 +8,15 @@ using namespace std;
 	Node *head;
 	Node *tail;
 }List;*/
-typedef struct Node {           //A node for the doubly linked list
-	// public:
-	int value;
-	Node *prev;
-	Node *next;
-}Node;
 
 class zDepthList {
+
+	typedef struct Node {           //A Node for the doubly linked list
+		// public:
+		int value;
+		Node *prev;
+		Node *next;
+	}Node;
 
 public:
     zDepthList();
@@ -23,10 +24,16 @@ public:
     void out(const char c);
     void out();
 	void move(int i, int j, char c);
+	void move(int i, int j);
+	void movefront(int i);
+	void moveback(int i);
+	int addback();
+	int addfront();
+	int at(int i);
 
 private:
     int listLength;
-	Node *head = new Node, *tail = new Node;      //Node in the zDepthList
+	Node *head, *tail;      //Node in the zDepthList
     int listIndexer[];                   //The array which holds indices of the list
 };
 
@@ -36,69 +43,70 @@ zDepthList :: zDepthList() {            //Default constructor
 }
 
 zDepthList :: zDepthList(int array[], int l) {        //Constructor with parameters
-    Node *nNode, *tempNode;
+    Node *nNode;
 	listLength = l;
     if(l == 0)
         cout << "An array of 0 length can't be made." << endl;
 
-    for(int i = 0; i <= l; i++) {
+    for(int i = 0; i < l; i++) {
 	    nNode = new Node;
 	    nNode->value = array[i];
-	    cout << tail->value;
-	    if ( i != l ) listIndexer[i] = i;      //Which is the index of the spot that [i] exists within the length of the linked list
-	    cout <<" ind " <<listIndexer[i];
-	    if(head == NULL) {        //If list is empty
-		    head  = nNode;    //The head points  to the new node
-		    tail  = nNode;    //The tail points to the new node
-		    nNode->prev = 0;   //New node doesn't point to anything
-		    nNode->next = 0;
+		nNode->next = NULL;
+		cout<<nNode->value;
+	    if ( i < l ) listIndexer[i] = i;      //Which is the index of the spot that [i] exists within the length of the linked list
+	    if(i==0) {        //If list is empty
+		    head = nNode;    //The head points  to the new Node
+		    tail = nNode;    //The tail points to the new Node
+		    nNode->prev = NULL;   //New Node doesn't point to anything
+			//cout << tail->value <<' ';
 		   }
 
         else if (1==1){
-            nNode->prev = tail;     	//Since the node is on the end, the new node points to the former end node
-			nNode->next = 0;			//The former end node points to the new node
-	        tail->next = nNode;			//new node points to nothing
-	        tail = nNode;
-                      //The tail points to the new end of the list
+			nNode->prev = tail;     	//Since the Node is on the end, the new Node points to the former end Node
+			nNode->next = 0;			//new Node points to nothing
+			tail->next = nNode;			//The former end Node points to the new
+	        tail = nNode;				//The tail points to the new end of the list
+
         }
    }
-
 }
 
 void zDepthList :: out(const char c) {
-	Node *currentNode = new Node;
+	Node *currentNode;
 	if ( c != 'r'){
 		currentNode = head;
-		cout << currentNode->value << ' ';
+		//cout << currentNode->value << ' ';
 		for (int i = 0; i < listLength; i++) {
-			currentNode = currentNode->next;
-			cout << currentNode->value << ' ';
+			//currentNode = currentNode->next;
+			//cout << currentNode->value << ' ';
 		}
 		cout << endl;
 	}
-	if (c == 'r'){
+
+	/* (c == 'r'){
+		cout<< "testr ";
 		currentNode = tail;
 		cout << currentNode->value << ' ';
 		for (int i = 0; i < listLength; i++){
 			currentNode = currentNode->prev;
-			cout << currentNode->value;
+			cout << currentNode->value << ' ';
 		}
 		cout << endl;
-	}
+	}*/
 }
 
 void zDepthList :: out() {
-	Node *currentNode = new Node;
+/*	Node *currentNode = new Node;
 	currentNode = head;
 	cout << currentNode->value << ' ';
 	for (int i = 0; i < listLength; i++) {
 		currentNode = currentNode->next;
 		cout << currentNode->value << ' ';
 	}
-	cout << endl;
+	cout << endl;*/
 }
 void zDepthList :: move(int i, int j, const char c) {
-	Node *holdIndex = new Node, *indexNode = new Node, *nextIndex = new Node, *prevIndex = new Node;
+	/*Node *holdIndex = new Node, *indexNode = new Node, *nextIndex = new Node, *prevIndex = new Node;
 	indexNode = head;
 	int location;
 	location = listIndexer[i];
@@ -123,6 +131,12 @@ void zDepthList :: move(int i, int j, const char c) {
 	else cout << "Invalid char entry"<<endl;
 	nextIndex = indexNode->next;
 	indexNode->next = holdIndex;
-	nextIndex->prev = holdIndex;
+	nextIndex->prev = holdIndex;*/
 
 }
+void zDepthList :: move(int i, int j){}
+void zDepthList :: movefront(int i){}
+void zDepthList :: moveback(int i){}
+int zDepthList :: addback(){}
+int zDepthList :: addfront(){}
+int zDepthList :: at(int i){}
